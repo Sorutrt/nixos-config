634fc287ab883edcfa78a399afc03cd039a275f2
sorutrt <kokishiono.58@gmail.com>
2026-01-23 03:02:46 +0000

    fix nix syntax error

--- home.nix
+++ home.nix
@@ -4,12 +4,12 @@
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
   home.stateVersion = "25.05";
-  home.packages = with pkgs; [
+  home.packages = (with pkgs; [
     nnn
     lazygit
-  ] ++ with unstablePkgs; [
+  ]) ++ (with unstablePkgs; [
     jujutsu
-  ];
+  ]);
 
   programs.home-manager.enable = true;
 }
