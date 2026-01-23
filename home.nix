c3c08707a0eed1d1000deac7d4a4a4d2168dbc83
sorutrt <kokishiono.58@gmail.com>
2026-01-23 05:30:53 +0000

    add some packages

--- home.nix
+++ home.nix
@@ -5,9 +5,13 @@
   home.homeDirectory = "/home/nixos";
   home.stateVersion = "25.05";
   home.packages = with pkgs; [
+    git
     nnn
     lazygit
     unstable.jujutsu
+    codex
+    wget
+    zsh
   ];
 
   programs.home-manager.enable = true;
